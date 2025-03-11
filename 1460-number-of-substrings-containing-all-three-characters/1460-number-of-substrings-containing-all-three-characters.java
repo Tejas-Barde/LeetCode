f29389d5ebb0937b93ef13cfee0a4f9cc8136cdf
Tejas-Barde <147363206+Tejas-Barde@users.noreply.github.com>
2025-03-11 13:03:33 +0530

    Time: 7 ms (99.47%), Space: 44.5 MB (79.55%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -1,22 +1,19 @@
 class Solution {
     public int numberOfSubstrings(String s) {
         int n = s.length();
-        Map<Character,Integer> map = new HashMap<>();
+        int[] map = new int[3];
         int right = 0,left = 0;
         int result = 0;
+        int size = 0;
         while(right < n){
             char c = s.charAt(right);
-            map.put(c,map.getOrDefault(c,0)+1);
-            if(map.size() == 3){
-                while(map.size() == 3){
-                    result += n-right;
-                    char lc = s.charAt(left);
-                    map.put(lc,map.get(lc)-1);
-                    if(map.get(lc) == 0) map.remove(lc);
-                    left++;
-                }
-                // System.out.println(left-1);
-                // System.out.println(result);
+            if(map[c-'a']++ == 0) size++;
+            while(size == 3){
+                result += n-right;
+                char lc = s.charAt(left);
+                map[lc-'a']--;
+                size = (map[lc-'a'] == 0)?size-1:size;
+                left++;
             }
             right++;
         }
